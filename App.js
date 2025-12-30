b0fc58362e6f6fb013bcb6af9388c77ae1c434d8
SUBHAJIT GHOSH <subhajitghosh@SUBHAJITs-Mac-mini.local>
2025-12-30 13:40:10 +0530

    update on 30-12-25

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { AuthProvider } from './src/context/AuthContext';
 import AppNav from './src/navigation/AppNav';
 import store from './src/store/store';
 import "./ignoreWarnings";
+import OfflineNotice from './src/utils/OfflineNotice';
 import Toast from 'react-native-toast-message';
 import SplashScreen from 'react-native-splash-screen';
 
@@ -23,6 +24,7 @@ function App() {
           backgroundColor="#000"
           barStyle={Platform.OS === 'ios' ? 'light-content' : 'light-content'}
         />
+        <OfflineNotice />
         <AuthProvider>
           <AppNav />
         </AuthProvider>
